b285e4ea58c338bd4b8357a5a925783b2db629a2
KenorAzka <156494844+KenorAzka@users.noreply.github.com>
2024-11-27 01:01:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -169,9 +169,9 @@ var init = function () {
       heartText.style.opacity = "0"; // Memudar
       setTimeout(() => {
         heartText.style.opacity = "1"; // Muncul kembali
-      }, 10000); // Waktu fade out
-    }, 10000); // Ulangi setiap 6 detik
-  }, 10000);
+      }, 6000); // Waktu fade out
+    }, 6000); // Ulangi setiap 6 detik
+  }, 6000);
 };
 
 // Jalankan inisialisasi
